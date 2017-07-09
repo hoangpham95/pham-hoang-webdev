f6db7457e8fc8d05ba2f6330193c8491bbf71b3a
hoangpham95 <phhoang@ccs.neu.edu>
2017-07-08 22:29:02 -0700

    changing db uri.

--- test/app.js
+++ test/app.js
@@ -7,7 +7,7 @@ app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    connectionString = process.env.MONGODB_URI
+    connectionString = process.env.MONGOLAB_AMBER_URI
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
